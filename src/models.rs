dec0d2893776b816849434b2685242e5f987ea06
maliByatzes <malib2027@gmail.com>
2024-01-05 22:22:45 +0200

    update models

--- src/models.rs
+++ src/models.rs
@@ -16,25 +16,3 @@ pub struct Guest {
     #[serde(rename = "updatedAt")]
     pub updated_at: Option<DateTime<Utc>>,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct TokenClaims {
-    pub sub: String,
-    pub iat: usize,
-    pub exp: usize,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct RegisterGuestSchema {
-    pub first_name: String,
-    pub last_name: String,
-    pub email_address: String,
-    pub password: String,
-    pub phone_number: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct LoginGuestSchema {
-    pub email_address: String,
-    pub password: String,
-}
